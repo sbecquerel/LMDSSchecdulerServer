91506d65f03ff68f864be33a75b837375ed96bbf
Sébastien Becquerel <s.becquerel@epiconcept.fr>
2019-08-17 22:59:31 +0200

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+const app = require('express')();
+const express = require('express');
+const server = require('http').createServer(app);
+const fs = require('fs');
+
+app
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+  .get('/calendar', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(fs.readFileSync('./calendar.json'));
+  })
+  .post('/save', (req, res) => {
+    if (req.body.teacherName === undefined
+      || req.body.ts === undefined
+      || req.body.studentName === undefined
+    ) {
+      return res.status(500).end();
+    }
+
+    fs.appendFileSync('result.csv', [
+      req.body.teacherName,
+      req.body.ts,
+      req.body.studentName
+    ].join(';') + "\n");
+
+    res.status(200).end()
+  })
+  .use((req, res, next) => {
+    res.status(404).end();
+  })
+  .listen(8081);
